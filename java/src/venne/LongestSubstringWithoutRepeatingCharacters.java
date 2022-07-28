e5c8a970b5eaec2226a18dd5675cc46c09961de5
urostripunovic <uros_1997@hotmail.com>
2022-07-28 11:45:57 +0200

    Done challenges and one on the way

---
+++ java/src/venne/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,15 @@
+package venne;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+		String s = "";
+		System.out.println(lengthOfLongestSubstring(s));
+	}
+
+	public static int lengthOfLongestSubstring(String s) {
+		int max = 0;
+		
+		return max;
+	}
+}
