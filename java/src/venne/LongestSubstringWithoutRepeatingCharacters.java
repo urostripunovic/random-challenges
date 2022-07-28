25b5f9f2732041812b27d398f106d1a30c92947f
urostripunovic <uros_1997@hotmail.com>
2022-07-28 13:51:51 +0200

    Update LongestSubstringWithoutRepeatingCharacters.java

--- java/src/venne/LongestSubstringWithoutRepeatingCharacters.java
+++ java/src/venne/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,15 +1,52 @@
 package venne;
 
 public class LongestSubstringWithoutRepeatingCharacters {
+	
+	/**
+	 * Given a string s, find the length of the longest substring without repeating characters.
+	 * 
+	 * Example 1:
+	 * Input: s = "abcabcbb"
+	 * Output: 3
+	 * Explanation: The answer is "abc", with the length of 3.
+	 * 
+	 * Example 2:
+	 * Input: s = "bbbbb"
+	 * Output: 1
+	 * Explanation: The answer is "b", with the length of 1.
+	 * 
+	 * Example 3:
+	 * Input: s = "pwwkew"
+	 * Output: 3
+	 * Explanation: The answer is "wke", with the length of 3.
+	 * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+	 */
 
 	public static void main(String[] args) {
-		String s = "";
+		String s = "pwwkew"; //dvdf
 		System.out.println(lengthOfLongestSubstring(s));
 	}
 
+	/**
+	 * Vi vill lägga till character om den inte finns i build strängen.
+	 * Finns den, då vill vi ta bort förgående och börja om på nytt med den nya karaktären i strängen build. 
+	 * Tänk på att inte ta bort all efter, exempel 3 va en liten fälla
+	 */
 	public static int lengthOfLongestSubstring(String s) {
-		int max = 0;
+		int longest = 0;
+		String build = "";
 		
-		return max;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (!build.contains(c+"")) {
+				build += c;
+				System.out.println(build);
+			} else {
+				//build = build.replace(c, c); //vi får med unwanted characters
+				build = build.substring(build.indexOf(c)+1)+c; 
+			}
+			longest = build.length() > longest ? build.length() : longest;
+		}		
+		return longest;
 	}
 }
